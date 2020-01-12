a6c95133dff28900f0b80b41f81d65ee5b5a38dc
ijgnd <fooijgnd@barijgnd>
2020-01-12 02:22:37 +0100

    add direct shortcuts

--- src/__init__.py
+++ src/__init__.py
@@ -18,7 +18,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 
-from anki.hooks import wrap 
+from anki.hooks import wrap, addHook
 
 from aqt import mw
 from aqt.browser import Browser
@@ -46,12 +46,16 @@ def onSearchEditTextChange(self, arg):
     vals = False
     c1 = gc("custom tag&deck string 1", "")
     c2 = gc("custom tag&deck string 2", "")
+    # vals = (remove some characters from the right of searchboxstring, 
+    #         InsertSpaceAtPos, 
+    #         UseFilterDialogKey(orValue),
+    #         ListForFilterDialog)
     if arg[-4:] == "tag:":
         if gc("modify_tag"):
-            vals = (False, True, self.col.tags.all())
+            vals = (False, -4, True, self.col.tags.all())
     elif arg[-5:] == "note:":
         if gc("modify_note"):
-            vals = (False, True, self.col.models.allNames())
+            vals = (False, -5, True, self.col.models.allNames())
     elif arg[-5:] == "card:":
         if gc("modify_card"):
             d = {}
@@ -59,28 +63,71 @@ def onSearchEditTextChange(self, arg):
                 modelname = m['name']
                 for t in m['tmpls']:
                     d[t['name'] + " (" + modelname + ")"] = t['name']
-            vals = (False, False, d)
+            vals = (False, -5, False, d)
     elif arg[-5:] == "deck:":
         if gc("modify_deck"):
-            vals = (False, True, sorted(self.col.decks.allNames()))
+            vals = (False, -5, True, sorted(self.col.decks.allNames()))
     elif c1 and arg[-len(c1):] == c1:
             alltags = ["tag:" + t for t in self.mw.col.tags.all()]
             decks = ["deck:" + d  for d in sorted(self.col.decks.allNames())]
-            vals = (-len(c1), True, alltags + decks)
+            vals = (-len(c1), 0, True, alltags + decks)
     elif c2 and arg[-len(c2):] == c2:
             alltags = ["tag:" + t for t in self.mw.col.tags.all()]
             decks = ["deck:" + d  for d in sorted(self.col.decks.allNames())]
-            vals = (-len(c2), True, alltags + decks)
+            vals = (-len(c2), 0, True, alltags + decks)
     if vals:
-        d = FilterDialog(parent=self, values=vals[2])
+        d = FilterDialog(parent=self, values=vals[3])
         if d.exec():
             if not vals[0]:
-                b = le.text()
+                t = le.text()
+                n = vals[1]
+                # multiple runs of insert_helper are still separated by spaces and in other 
+                # situations this makes the search more readable
+                b = t[:n] + " " + t[n:]  
             else:
                 b = le.text()[:vals[0]]
-            if vals[1]:
+            if vals[2]:
                 i = d.selkey
             else:
                 i = d.selvalue
             le.setText(b + '"' + i + '"')
 Browser.onSearchEditTextChange = onSearchEditTextChange
+
+
+def insert_helper(self, val):
+    l = self.form.searchEdit.lineEdit()
+    if l.text() == self._searchPrompt:
+        l.setText(val)
+    else:
+        l.setText(l.text() + val)
+    mod = self.mw.app.keyboardModifiers() & Qt.ShiftModifier
+    if gc("shortcuts trigger search by default"):
+        if not mod:
+            self.onSearchActivated()
+    else:
+        if mod:
+            self.onSearchActivated()
+
+
+def setupMenu(browser):
+    c1 = gc("shortcut - focus search box and card selector dialog")
+    if c1:
+        a = QShortcut(QKeySequence(c1), browser)
+        a.activated.connect(lambda b=browser: insert_helper(b, "card:"))
+    c2 = gc("shortcut - focus search box and note selector dialog")
+    if c2:
+        b = QShortcut(QKeySequence(c2), browser)
+        b.activated.connect(lambda b=browser: insert_helper(b, "note:"))
+    c3 = gc("shortcut - focus search box and tag selector dialog")
+    if c3:
+        c = QShortcut(QKeySequence(c3), browser)
+        c.activated.connect(lambda b=browser: insert_helper(b, "tag:"))
+    c4 = gc("shortcut - focus search box and tag/deck selector dialog")
+    if c4:
+        d = QShortcut(QKeySequence(c4), browser)
+        d.activated.connect(lambda b=browser: insert_helper(b, gc("custom tag&deck string 1", "xx")))
+    c5 = gc("shortcut - focus search box and deck selector dialog")
+    if c5:
+        e = QShortcut(QKeySequence(c5), browser)
+        e.activated.connect(lambda b=browser: insert_helper(b, "deck:"))
+addHook("browser.setupMenus", setupMenu)
