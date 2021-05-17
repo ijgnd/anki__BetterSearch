4087a664188cd129f95eea670192261cfb9d5d46
ijgnd <fooijgnd@barijgnd>
2021-05-18 01:52:28 +0200

    quick first fix for 2.1.45, #28

--- src/ui_filtered_decks_dialog.py
+++ src/ui_filtered_decks_dialog.py
@@ -1,5 +1,6 @@
 from anki.hooks import wrap
-from aqt.dyndeckconf import DeckConf
+
+from anki.utils import pointVersion
 
 from .onTextChange import onSearchEditTextChange
 
@@ -7,29 +8,63 @@ from .onTextChange import onSearchEditTextChange
 # TODO setup shortcuts
 
 
-def dyn_setup_search(self):
-    self.form.search.textChanged.connect(self.onDynSetupSearchEditTextChange)
-    self.form.search_2.textChanged.connect(self.onDynSetupSearchEditTextChange)
-DeckConf.initialSetup = wrap(DeckConf.initialSetup, dyn_setup_search)
+if pointVersion() <= 44:
+    from aqt.dyndeckconf import DeckConf
+
+    def dyn_setup_search(self):
+        self.form.search.textChanged.connect(self.onDynSetupSearchEditTextChange)
+        self.form.search_2.textChanged.connect(self.onDynSetupSearchEditTextChange)
+    DeckConf.initialSetup = wrap(DeckConf.initialSetup, dyn_setup_search)
 
 
-def onDynSetupSearchEditTextChange(self, arg):
-    le = self.sender()  # https://stackoverflow.com/a/33981172
-    pos = le.cursorPosition()
-    ret = onSearchEditTextChange(
-        parent=self,
-        move_dialog_in_browser=False,
-        include_filtered_in_deck=False,
-        func_gettext=le.text,
-        func_settext=le.setText,
-        cursorpos=pos,
-        mw=self.mw,
-        col=self.mw.col
-    )
-    if not ret:
-        return
-    else:
-        newpos, triggersearch = ret
-        if newpos:
-            le.setCursorPosition(newpos)
-DeckConf.onDynSetupSearchEditTextChange = onDynSetupSearchEditTextChange
+    def onDynSetupSearchEditTextChange(self, arg):
+        le = self.sender()  # https://stackoverflow.com/a/33981172
+        pos = le.cursorPosition()
+        ret = onSearchEditTextChange(
+            parent=self,
+            move_dialog_in_browser=False,
+            include_filtered_in_deck=False,
+            func_gettext=le.text,
+            func_settext=le.setText,
+            cursorpos=pos,
+            mw=self.mw,
+            col=self.mw.col
+        )
+        if not ret:
+            return
+        else:
+            newpos, triggersearch = ret
+            if newpos:
+                le.setCursorPosition(newpos)
+    DeckConf.onDynSetupSearchEditTextChange = onDynSetupSearchEditTextChange
+
+
+else:
+    from aqt.filtered_deck import FilteredDeckConfigDialog
+
+    def dyn_setup_search(self):
+        self.form.search.textChanged.connect(self.onDynSetupSearchEditTextChange)
+        self.form.search_2.textChanged.connect(self.onDynSetupSearchEditTextChange)
+    FilteredDeckConfigDialog._initial_dialog_setup = wrap(FilteredDeckConfigDialog._initial_dialog_setup, dyn_setup_search)
+
+
+    def onDynSetupSearchEditTextChange(self, arg):
+        le = self.sender()  # https://stackoverflow.com/a/33981172
+        pos = le.cursorPosition()
+        ret = onSearchEditTextChange(
+            parent=self,
+            move_dialog_in_browser=False,
+            include_filtered_in_deck=False,
+            func_gettext=le.text,
+            func_settext=le.setText,
+            cursorpos=pos,
+            mw=self.mw,
+            col=self.mw.col
+        )
+        if not ret:
+            return
+        else:
+            newpos, triggersearch = ret
+            if newpos:
+                le.setCursorPosition(newpos)
+    FilteredDeckConfigDialog.onDynSetupSearchEditTextChange = onDynSetupSearchEditTextChange
